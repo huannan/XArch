914728cf2f5d1ae5f2628e155b65ba0b83b93247
吴焕楠 <wuhuannan@meizu.com>
2021-09-16 15:45:44 +0800

    opt oncreate

--- app/src/main/java/com/nan/xarch/XArchApplication.kt
+++ app/src/main/java/com/nan/xarch/XArchApplication.kt
@@ -1,6 +1,8 @@
 package com.nan.xarch
 
+import android.app.ActivityManager
 import android.app.Application
+import android.os.Process
 import com.nan.xarch.persistence.XKeyValue
 
 /**
@@ -10,8 +12,24 @@ class XArchApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        instance = this
-        XKeyValue.init(this)
+        initInMainProcess {
+            instance = this
+            XKeyValue.init(this)
+        }
+    }
+
+    /**
+     * 主进程初始化
+     */
+    private fun initInMainProcess(block: () -> Unit) {
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        val myPId = Process.myPid()
+        val mainProcessName = packageName
+        activityManager.runningAppProcesses.forEach {
+            if (it.pid == myPId && it.processName == mainProcessName) {
+                block()
+            }
+        }
     }
 
     companion object {
