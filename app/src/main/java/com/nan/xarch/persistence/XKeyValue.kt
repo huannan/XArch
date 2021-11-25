3ac3f2557cb12867d202a29ce07ee497a2c43d90
吴焕楠 <wuhuannan@meizu.com>
2021-11-25 09:20:24 +0800

    sync code

--- app/src/main/java/com/nan/xarch/persistence/XKeyValue.kt
+++ app/src/main/java/com/nan/xarch/persistence/XKeyValue.kt
@@ -10,79 +10,183 @@ import com.tencent.mmkv.MMKV
  */
 object XKeyValue {
 
+    private const val ID_DEFAULT = "id_default"
+    private const val ID_ACCOUNT = "id_account"
+
     fun init(application: Application) {
         MMKV.initialize(application)
     }
 
-    fun putBoolean(@Key key: String, value: Boolean) {
-        MMKV.defaultMMKV().encode(key, value)
+    fun getDefaultMMKV(): MMKV {
+        return MMKV.mmkvWithID(ID_DEFAULT, MMKV.SINGLE_PROCESS_MODE)
     }
 
+    fun getAccountMMKV(): MMKV {
+        return MMKV.mmkvWithID(ID_ACCOUNT, MMKV.SINGLE_PROCESS_MODE)
+    }
+
+    fun clear() {
+        MMKV.defaultMMKV().clearMemoryCache()
+        MMKV.defaultMMKV().clearAll()
+    }
+
+    fun putBoolean(@Key key: String, value: Boolean) {
+        putBoolean(getDefaultMMKV(), key, value)
+    }
+
+    fun putBoolean(mmkv: MMKV, @Key key: String, value: Boolean) {
+        mmkv.encode(key, value)
+    }
+
+    @JvmOverloads
     fun getBoolean(@Key key: String, defaultValue: Boolean = false): Boolean {
-        return MMKV.defaultMMKV().decodeBool(key, defaultValue)
+        return getBoolean(getDefaultMMKV(), key, defaultValue)
+    }
+
+    @JvmOverloads
+    fun getBoolean(mmkv: MMKV, @Key key: String, defaultValue: Boolean = false): Boolean {
+        return mmkv.decodeBool(key, defaultValue)
     }
 
     fun putString(@Key key: String, value: String) {
-        MMKV.defaultMMKV().encode(key, value)
+        putString(getDefaultMMKV(), key, value)
     }
 
+    fun putString(mmkv: MMKV, @Key key: String, value: String) {
+        mmkv.encode(key, value)
+    }
+
+    @JvmOverloads
     fun getString(@Key key: String, defaultValue: String = ""): String {
-        return MMKV.defaultMMKV().decodeString(key, defaultValue)!!
+        return getString(getDefaultMMKV(), key, defaultValue)
+    }
+
+    @JvmOverloads
+    fun getString(mmkv: MMKV, @Key key: String, defaultValue: String = ""): String {
+        return mmkv.decodeString(key, defaultValue)!!
     }
 
     fun putInt(@Key key: String, value: Int) {
-        MMKV.defaultMMKV().encode(key, value)
+        putInt(getDefaultMMKV(), key, value)
     }
 
+    fun putInt(mmkv: MMKV, @Key key: String, value: Int) {
+        mmkv.encode(key, value)
+    }
+
+    @JvmOverloads
     fun getInt(@Key key: String, defaultValue: Int = 0): Int {
-        return MMKV.defaultMMKV().decodeInt(key, defaultValue)
+        return getInt(getDefaultMMKV(), key, defaultValue)
+    }
+
+    @JvmOverloads
+    fun getInt(mmkv: MMKV, @Key key: String, defaultValue: Int = 0): Int {
+        return mmkv.decodeInt(key, defaultValue)
     }
 
     fun putFloat(@Key key: String, value: Float) {
-        MMKV.defaultMMKV().encode(key, value)
+        putFloat(getDefaultMMKV(), key, value)
     }
 
+    fun putFloat(mmkv: MMKV, @Key key: String, value: Float) {
+        mmkv.encode(key, value)
+    }
+
+    @JvmOverloads
     fun getFloat(@Key key: String, defaultValue: Float = 0F): Float {
-        return MMKV.defaultMMKV().decodeFloat(key, defaultValue)
+        return getFloat(getDefaultMMKV(), key, defaultValue)
+    }
+
+    @JvmOverloads
+    fun getFloat(mmkv: MMKV, @Key key: String, defaultValue: Float = 0F): Float {
+        return mmkv.decodeFloat(key, defaultValue)
     }
 
     fun putLong(@Key key: String, value: Long) {
-        MMKV.defaultMMKV().encode(key, value)
+        putLong(getDefaultMMKV(), key, value)
     }
 
+    fun putLong(mmkv: MMKV, @Key key: String, value: Long) {
+        mmkv.encode(key, value)
+    }
+
+    @JvmOverloads
     fun getLong(@Key key: String, defaultValue: Long = 0L): Long {
-        return MMKV.defaultMMKV().decodeLong(key, defaultValue)
+        return getLong(getDefaultMMKV(), key, defaultValue)
+    }
+
+    @JvmOverloads
+    fun getLong(mmkv: MMKV, @Key key: String, defaultValue: Long = 0L): Long {
+        return mmkv.decodeLong(key, defaultValue)
     }
 
     fun putDouble(@Key key: String, value: Double) {
-        MMKV.defaultMMKV().encode(key, value)
+        putDouble(getDefaultMMKV(), key, value)
     }
 
+    fun putDouble(mmkv: MMKV, @Key key: String, value: Double) {
+        mmkv.encode(key, value)
+    }
+
+    @JvmOverloads
     fun getDouble(@Key key: String, defaultValue: Double = 0.0): Double {
-        return MMKV.defaultMMKV().decodeDouble(key, defaultValue)
+        return getDouble(getDefaultMMKV(), key, defaultValue)
+    }
+
+    @JvmOverloads
+    fun getDouble(mmkv: MMKV, @Key key: String, defaultValue: Double = 0.0): Double {
+        return mmkv.decodeDouble(key, defaultValue)
     }
 
     fun putByteArray(@Key key: String, value: ByteArray) {
-        MMKV.defaultMMKV().encode(key, value)
+        putByteArray(getDefaultMMKV(), key, value)
     }
 
+    fun putByteArray(mmkv: MMKV, @Key key: String, value: ByteArray) {
+        mmkv.encode(key, value)
+    }
+
+    @JvmOverloads
     fun getByteArray(@Key key: String, defaultValue: ByteArray = ByteArray(0)): ByteArray {
-        return MMKV.defaultMMKV().decodeBytes(key, defaultValue)!!
+        return getByteArray(getDefaultMMKV(), key, defaultValue)
+    }
+
+    @JvmOverloads
+    fun getByteArray(mmkv: MMKV, @Key key: String, defaultValue: ByteArray = ByteArray(0)): ByteArray {
+        return mmkv.decodeBytes(key, defaultValue)!!
     }
 
     fun putStringSet(@Key key: String, value: Set<String>) {
-        MMKV.defaultMMKV().encode(key, value)
+        putStringSet(getDefaultMMKV(), key, value)
     }
 
+    fun putStringSet(mmkv: MMKV, @Key key: String, value: Set<String>) {
+        mmkv.encode(key, value)
+    }
+
+    @JvmOverloads
     fun getStringSet(@Key key: String, defaultValue: Set<String> = mutableSetOf()): Set<String> {
-        return MMKV.defaultMMKV().decodeStringSet(key, defaultValue)!!
+        return getStringSet(getDefaultMMKV(), key, defaultValue)
+    }
+
+    @JvmOverloads
+    fun getStringSet(mmkv: MMKV, @Key key: String, defaultValue: Set<String> = mutableSetOf()): Set<String> {
+        return mmkv.decodeStringSet(key, defaultValue)!!
     }
 
     fun putParcelable(@Key key: String, value: Parcelable) {
-        MMKV.defaultMMKV().encode(key, value)
+        putParcelable(getDefaultMMKV(), key, value)
+    }
+
+    fun putParcelable(mmkv: MMKV, @Key key: String, value: Parcelable) {
+        mmkv.encode(key, value)
     }
 
     inline fun <reified T : Parcelable> getParcelable(@Key key: String): T? {
-        return MMKV.defaultMMKV().decodeParcelable(key, T::class.java)
+        return getParcelable(getDefaultMMKV(), key)
+    }
+
+    inline fun <reified T : Parcelable> getParcelable(mmkv: MMKV, @Key key: String): T? {
+        return mmkv.decodeParcelable(key, T::class.java)
     }
 }
